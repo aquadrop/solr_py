86c918aefb1cf899dc888a9cf3a3caae773f34af
Jianwei Xu <forestserenity@gmail.com>
2017-07-24 13:59:18 +0800

    fix

--- client/gkernel.py
+++ client/gkernel.py
@@ -170,6 +170,7 @@ class GKernel:
         if given_slot == '#NULL#':
             given_slot = None
             self.last_slot = None
+            self.clear_state()
         if self.state_cleared:
             if given_slot:
                 self.should_clear_state(self.last_slot)
