05bb5d83a5d56eb136bea3eefc188ccaaa4698dc
flowcb <cbandhy@gmail.com>
2017-08-16 09:54:43 +0800

    add negative

---
+++ client/sc_negative_clf.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+from cn_util import print_cn
+
+
+
+negative_pattern = re.compile(ur'.*?(不要|不买|不是|不吃|不太|不喜欢|不爱|吃不起|买不起).*?')
+
+class Negative_Clf:
+    def __init__(self):
+        pass
+
+    def predict(self,input_):
+        try:
+            input_=input_.decode('utf-8')
+            if re.match(negative_pattern, input_):
+                return True
+            else:
+                return False
+        except:
+            return False
+
