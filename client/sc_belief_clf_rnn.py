003aca9a8d7bd73dfbe15dd4cdbe3e066d365040
flowcb <cbandhy@gmail.com>
2017-08-25 14:09:14 +0800

    add fasttext embedding

--- client/sc_belief_clf_rnn.py
+++ client/sc_belief_clf_rnn.py
@@ -8,6 +8,8 @@ from __future__ import print_function
 import os
 import sys
 import json
+import jieba
+import requests
 import argparse
 from cn_util import print_cn
 from itertools import chain
@@ -24,12 +26,14 @@ from utils.mail import send_mail
 
 ENC_VOCAB_SIZE = 3694
 DEC_VOCAB_SIZE = 260
-EMBEDDING_SIZE = 400
+EMBEDDING_SIZE = 300
 HIDDEN_UNIT = 256
 N_LAYER = 10
 BATCH_SIZE = 32
 RNN_SIZE = 256
 PAD = 2
+
+url = 'http://localhost:11425/fasttext/w2v?q='
 
 char2index = None
 index2char = None
@@ -56,14 +60,14 @@ def init_dict(dict_path):
 
 def recover(index, source=True):
     sentence = []
-    ignore = [0, 2, 3]
+    ignore = ['#PAD#', '#UNK#', '#GO#', 0, 1, 2]
     for ii in index:
         if ii in ignore:
             continue
-        if ii == 1:
+        if ii in ['#EOS#', 1]:
             break
         if source:
-            sentence.append(str(index2char[str(ii)]))
+            sentence.append(ii)
         else:
             sentence.append(str(index2label[str(ii)]))
     return ''.join(sentence)
@@ -83,6 +87,46 @@ def padding(inputs):
     return inputs_batch_major
 
 
+def padding2(inputs):
+    batch_size = len(inputs)
+    sequence_lengths = [len(seq) for seq in inputs]
+    max_sequence_length = max(sequence_lengths)
+
+    result = list()
+    for input in inputs:
+        input.extend(['#PAD#'] * (max_sequence_length - len(input)))
+        result.append(input)
+
+    return result
+
+
+def fasttext_vector(tokens):
+    global url
+    ff_url = url + ','.join(tokens).decode('utf-8')
+    r = requests.get(url=ff_url)
+    vector = r.json()['vector']
+    return vector
+
+
+def fasttext_wv(word):
+    global url
+    ff_url = url + word.decode('utf-8')
+    r = requests.get(url=ff_url)
+    vector = r.json()['vector']
+    return vector
+
+
+def embedding(inputs):
+    embeddings = list()
+    for inp in inputs:
+        embedding = list()
+        for word in inp:
+            resp = fasttext_wv(word)
+            embedding.append(resp)
+        embeddings.append(embedding)
+    return np.squeeze(np.asarray(embeddings))
+
+
 def generate_batch_data(data_path, batch_size=32):
     sources = list()
     targets = list()
@@ -96,34 +140,37 @@ def generate_batch_data(data_path, batch_size=32):
             source = line.split('#')[1]
             sources.append(source)
             targets.append(target)
-            # print_cn(target)
     size = len(sources)
 
     while True:
         ss = np.random.randint(0, size, batch_size)
 
-        encoder_inputs = [
-            [char2index.get(word, 3) for word in sources[line_num].strip().decode('utf-8')] for line_num in
-            ss]
-        decoder_inputs = [
-            [label2index.get('#GO#')] + [label2index.get(word.decode('utf-8'), 3) for word in targets[line_num]]
-            for
-            line_num in ss]
-        labels = [
-            [label2index.get(word.decode('utf-8'), 3) for word in targets[line_num]] + [label2index.get('#EOS#')]
-            for
-            line_num in ss]
+        encoder_inputs = list()
+        decoder_inputs = list()
+        labels = list()
+
+        for ln in ss:
+            source_tokens = list(jieba.cut(sources[ln].strip().decode('utf-8')))
+            target_tokens = targets[ln]
+            encoder_input = ['#GO#'] + target_tokens
+            label = target_tokens + ['#EOS#']
+            encoder_inputs.append(source_tokens)
+            decoder_inputs.append(encoder_input)
+            labels.append(label)
+
         encoder_inputs_length = [len(line) for line in encoder_inputs]
         decoder_inputs_length = [len(line) for line in decoder_inputs]
 
-        encoder_inputs = padding(encoder_inputs)
-        decoder_inputs = padding(decoder_inputs)
-        labels = padding(labels)
+        encoder_inputs = padding2(encoder_inputs)
+        decoder_inputs = padding2(decoder_inputs)
+        labels = padding2(labels)
 
-        yield np.asarray(encoder_inputs), np.asarray(decoder_inputs), np.asarray(labels), np.asarray(
+        encoder_embed_inputs = embedding(encoder_inputs)
+        decoder_embed_inputs = embedding(decoder_inputs)
+        labelss = [[label2index.get(word.decode('utf-8'), 3) for word in ln] for ln in labels]
+
+        yield encoder_inputs, decoder_inputs, encoder_embed_inputs, decoder_embed_inputs, np.array(labelss), np.asarray(
             encoder_inputs_length), np.asarray(decoder_inputs_length)
-
-        # yield encoder_inputs, decoder_inputs, labels, encoder_inputs_length, decoder_inputs_length
 
 
 class BeliefRnn:
@@ -135,16 +182,24 @@ class BeliefRnn:
 
         self.labels_ = tf.placeholder(
             tf.int32, shape=(None, None), name='labels_')
+
         with tf.variable_scope("encoder") as scope:
-            self.encoder_inputs = tf.placeholder(
-                tf.int32, shape=(None, None), name="encoder_inputs")
+            self.encoder_embed_inputs = tf.placeholder(
+                tf.float32, shape=(None, None, EMBEDDING_SIZE), name="encoder_embed_inputs")
             self.encoder_inputs_length = tf.placeholder(
                 tf.int32, shape=(None,), name="encoder_inputs_length")
+
         with tf.variable_scope("decoder") as scope:
-            self.decoder_inputs = tf.placeholder(
-                tf.int32, shape=(None, None), name="decoder_inputs")
+            self.decoder_embed_inputs = tf.placeholder(
+                tf.float32, shape=(None, None, EMBEDDING_SIZE), name="encoder_embed_inputs")
             self.decoder_inputs_length = tf.placeholder(
                 tf.int32, shape=(None,), name="decoder_inputs_length")
+
+        with tf.variable_scope("predict") as scope:
+            self.predicting_embed_inputs = tf.placeholder(
+                tf.float32, shape=(None, None, EMBEDDING_SIZE), name="predicting_embed_inputs")
+            self.predicting_inputs_length= tf.placeholder(
+                tf.int32, shape=(None,), name="predicting_inputs_length")
 
     def _inference(self):
         print('Create inference......')
@@ -154,18 +209,15 @@ class BeliefRnn:
             return cell
 
         with tf.variable_scope("encoder") as scope:
-            encoder_embeddings = tf.Variable(tf.random_uniform([ENC_VOCAB_SIZE, EMBEDDING_SIZE]))
-            encoder_embed_input = tf.contrib.layers.embed_sequence(self.encoder_inputs, ENC_VOCAB_SIZE, EMBEDDING_SIZE)
             encoder_cell = tf.contrib.rnn.MultiRNNCell([get_cell(RNN_SIZE) for _ in range(N_LAYER)])
-            encoder_output, encoder_state = tf.nn.dynamic_rnn(encoder_cell, encoder_embed_input,
+            encoder_output, encoder_state = tf.nn.dynamic_rnn(encoder_cell, self.encoder_embed_inputs,
                                                               sequence_length=self.encoder_inputs_length,
                                                               dtype=tf.float32)
-
-
+        with tf.variable_scope("encoder") as scope:
+            predicting_encoder_output, predicting_encoder_state = tf.nn.dynamic_rnn(encoder_cell, self.predicting_embed_inputs,
+                                                              sequence_length=self.predicting_inputs_length,
+                                                              dtype=tf.float32)
         with tf.variable_scope("decoder") as scope:
-            decoder_embeddings = tf.Variable(tf.random_uniform([DEC_VOCAB_SIZE, EMBEDDING_SIZE]))
-            decoder_embed_input = tf.nn.embedding_lookup(decoder_embeddings, self.decoder_inputs)
-            # decoder_embed_input = tf.contrib.layers.embed_sequence(self.decoder_inputs, DEC_VOCAB_SIZE, EMBEDDING_SIZE)
             decoder_cell = tf.contrib.rnn.MultiRNNCell([get_cell(RNN_SIZE) for _ in range(N_LAYER)])
 
             output_layer = Dense(DEC_VOCAB_SIZE,
@@ -174,7 +226,7 @@ class BeliefRnn:
                 self.decoder_inputs_length, name='max_target_len')
 
         with tf.variable_scope("decoder") as scope:
-            training_helper = tf.contrib.seq2seq.TrainingHelper(inputs=decoder_embed_input,
+            training_helper = tf.contrib.seq2seq.TrainingHelper(inputs=self.decoder_embed_inputs,
                                                                 sequence_length=self.decoder_inputs_length,
                                                                 time_major=False)
 
@@ -186,16 +238,16 @@ class BeliefRnn:
             self.decoder_output, decoder_state, _ = tf.contrib.seq2seq.dynamic_decode(training_decoder,
                                                                                       impute_finished=True,
                                                                                       maximum_iterations=self.max_target_sequence_length)
+            # self.training_logits_ = tf.identity(self.decoder_output.rnn_output, 'logits')
 
         with tf.variable_scope("decoder", reuse=True):
-            start_tokens = tf.tile(tf.constant([label2index['#GO#']], dtype=tf.int32), [BATCH_SIZE],
-                                   name='start_tokens')
-            predicting_helper = tf.contrib.seq2seq.GreedyEmbeddingHelper(encoder_embeddings,
-                                                                         start_tokens,
-                                                                         label2index['#EOS#'])
+
+            predicting_helper = tf.contrib.seq2seq.TrainingHelper(inputs=self.predicting_embed_inputs,
+                                                                sequence_length=self.predicting_inputs_length,
+                                                                time_major=False)
             predicting_decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell,
                                                                  predicting_helper,
-                                                                 encoder_state,
+                                                                 predicting_encoder_state,
                                                                  output_layer)
             self.predicting_output, _, _ = tf.contrib.seq2seq.dynamic_decode(predicting_decoder,
                                                                              impute_finished=True)
@@ -223,6 +275,15 @@ class BeliefRnn:
         if self.trainable:
             self._create_loss()
             self._create_optimizer()
+
+    def build_graph_debug(self):
+        print('Build graph......')
+
+        self._create_placeholder()
+        self._inference()
+        # if self.trainable:
+        #     self._create_loss()
+        #     self._create_optimizer()
 
 
 def _check_restore_parameters(sess, saver):
@@ -257,13 +318,15 @@ def train(data_path, dict_path):
         _check_restore_parameters(sess, saver)
         max_loss = 0.5
         i = 0
-        for enci, deci, lab, encil, decil in gen:
-            # print(enci)
-            # print(deci)
-            # print(lab)
-            model.optimizer.run(feed_dict={model.encoder_inputs.name: enci,
+        for enci, deci, enci_emb, deci_emb, lab, encil, decil in gen:
+            # print(enci[0])
+            # print(deci[0])
+            # print(enci_emb.shape)
+            # print(deci_emb.shape)
+            # print(lab[0])
+            model.optimizer.run(feed_dict={model.encoder_embed_inputs.name: enci_emb,
                                            model.encoder_inputs_length.name: encil,
-                                           model.decoder_inputs.name: deci,
+                                           model.decoder_embed_inputs.name: deci_emb,
                                            model.decoder_inputs_length.name: decil,
                                            model.labels_.name: lab})
 
@@ -271,16 +334,16 @@ def train(data_path, dict_path):
                 loss, predictions, logits, c = sess.run(
                     [model.loss, model.predictions_,
                      model.training_logits_, model.labels_],
-                    feed_dict={model.encoder_inputs.name: enci,
+                    feed_dict={model.encoder_embed_inputs.name: enci_emb,
                                model.encoder_inputs_length.name: encil,
-                               model.decoder_inputs.name: deci,
+                               model.decoder_embed_inputs.name: deci_emb,
                                model.decoder_inputs_length.name: decil,
                                model.labels_.name: lab})
 
                 print('---------------------------------------')
                 print("step", i)
                 print('question:     >', recover(enci[0]))
-                print('answer:       >', recover(deci[0], False))
+                print('answer:       >', recover(deci[0]))
                 # print('prediction:   >', predictions[0])
                 print('prediction:   >', recover(predictions[0], False))
                 print('loss:         >', loss)
@@ -296,15 +359,15 @@ def train(data_path, dict_path):
                     loss, predictions, logits, c = sess.run(
                         [model.loss, model.predictions_,
                          model.training_logits_, model.labels_],
-                        feed_dict={model.encoder_inputs.name: enci,
+                        feed_dict={model.encoder_embed_inputs.name: enci_emb,
                                    model.encoder_inputs_length.name: encil,
-                                   model.decoder_inputs.name: deci,
+                                   model.decoder_embed_inputs.name: deci_emb,
                                    model.decoder_inputs_length.name: decil,
                                    model.labels_.name: lab})
 
                     sends = 'step ' + str(i) + '\n' \
                             + 'question:     >' + recover(enci[0]) + '\n' \
-                            + 'answer:       >' + recover(deci[0], False) + '\n' \
+                            + 'answer:       >' + recover(deci[0]) + '\n' \
                             + 'prediction:   >' + recover(predictions[0], False) + '\n' \
                             + 'loss:         >' + str(loss)
 
@@ -326,37 +389,45 @@ def predict(dict_path):
         _check_restore_parameters(sess, saver)
         while True:
             line = _get_user_input()
-            translation = [char2index.get(word, 3) for word in line.strip().decode('utf-8')]
-            translation = [char2index.get('#GO#')] + translation
-            encoder_input = np.tile([translation], (BATCH_SIZE, 1))
-            encoder_lengths = np.array([len(o) for o in encoder_input])
-
-            # print(encoder_input.shape)
-
-            # logits = loaded_graph.get_tensor_by_name('predictions:0')
+            predicting_inputs=list(jieba.cut(line.strip().decode('utf-8')))
+            print_cn(predicting_inputs)
+            predicting_inputs_length = [len(predicting_inputs)]*BATCH_SIZE
+            predicting_embed_inputs=[[fasttext_wv(word) for word in predicting_inputs] for _ in range(BATCH_SIZE)]
+            predicting_embed_inputs=np.asarray(predicting_embed_inputs)
 
             answer_logits = sess.run(model.predicting_logits,
-                                     feed_dict={model.encoder_inputs.name: encoder_input,
-                                                model.encoder_inputs_length.name: encoder_lengths})
+                                     feed_dict={model.predicting_embed_inputs.name: predicting_embed_inputs,
+                                                model.predicting_inputs_length.name: predicting_inputs_length})
 
-            prediction = recover(answer_logits.tolist()[0],False)
-            # print_cn(prediction)
+            prediction = recover(answer_logits.tolist()[0], False)
+            # print(answer_logits.tolist()[0])
             print("predict->", prediction)
             print("-----------------------")
-
-def interface(query):
-
 
 
 def debug():
     dict_path = ['../data/sc/dict/char2index_dict_big.txt', '../data/sc/dict/index2char_dict_big.txt',
                  '../data/sc/dict/label2index.txt', '../data/sc/dict/index2label.txt']
+    data_path = '../data/sc/train/sale_v2.txt'
+    print('Training......')
+
     init_dict(dict_path)
-    gen = generate_batch_data('../data/sc/train/sale_v2.txt')
-    for enci, deci, lab, encil, decil in gen:
-        print(enci)
-        print(deci)
-        print(lab)
+    gen = generate_batch_data(data_path)
+    model = BeliefRnn()
+    model.build_graph_debug()
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for enci, deci, lab, encil, decil in gen:
+            # print(enci)
+            training_logits_, decoder_output = sess.run(
+                [model.training_logits_, model.decoder_output],
+                feed_dict={model.encoder_embed_inputs.name: enci,
+                           model.encoder_inputs_length.name: encil,
+                           model.decoder_embed_inputs.name: deci,
+                           model.decoder_inputs_length.name: decil})
+            print(training_logits_.shape)
+            print(decoder_output.shape)
 
 
 def main():
